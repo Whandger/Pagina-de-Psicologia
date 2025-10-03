99e4340b9a9ae423c5dd022fdcf12cf572af6e78
Whandger <whandgerw@yahoo.com.br>
2025-10-03 14:09:26 -0300

    Add files via upload
    Making use of cdn for image server

--- js/icon.js
+++ js/icon.js
@@ -2,7 +2,7 @@ const favicon = document.getElementById("favicon");
 
 // Quando o usuário sai da aba
 window.addEventListener("blur", () => {
-    favicon.href = "./images/logo-white.png"; // ícone alternativo
+    favicon.href = "https://res.cloudinary.com/deygsv2fm/image/upload/v1759499511/logo-white_yk7of1.png"; // ícone alternativo
 });
 
 // Quando o usuário volta pra aba
